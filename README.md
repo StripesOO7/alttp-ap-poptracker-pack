6855853e562365f98ff02604fc42633866733820
StripesOO7 <m-holtschke1@web.de>
2023-05-23 14:57:58 +0200

    dungeon json split

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Emerald AP Tracker
 
-ArchipelagoA Link to the Past tracker pack for [PopTracker](https://github.com/black-sliver/PopTracker/) with Autotracking.
+Archipelago A Link to the Past tracker pack for [PopTracker](https://github.com/black-sliver/PopTracker/) with Autotracking.
 
 PopTracker v0.23.0 or higher is recommended.
--- scripts/init.lua
+++ scripts/init.lua
@@ -12,9 +12,22 @@ else
     Tracker:AddMaps("maps/maps.json")  
 end  
 -- Locations
-Tracker:AddLocations("locations/locations.json")
-Tracker:AddLocations("locations/lightworld.json")
 Tracker:AddLocations("locations/darkworld.json")
+Tracker:AddLocations("locations/lightworld.json")
+Tracker:AddLocations("locations/HC.json")
+Tracker:AddLocations("locations/AT.json")
+Tracker:AddLocations("locations/EP.json")
+Tracker:AddLocations("locations/DP.json")
+Tracker:AddLocations("locations/ToH.json")
+Tracker:AddLocations("locations/PoD.json")
+Tracker:AddLocations("locations/SP.json")
+Tracker:AddLocations("locations/SW.json")
+Tracker:AddLocations("locations/TT.json")
+Tracker:AddLocations("locations/MM.json")
+Tracker:AddLocations("locations/IP.json")
+Tracker:AddLocations("locations/TR.json")
+Tracker:AddLocations("locations/GT.json")
+
 if PopVersion and PopVersion >= "0.23.0" then
     Tracker:AddLocations("locations/dungeons.json")
 end
