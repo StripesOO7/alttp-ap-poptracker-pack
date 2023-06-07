666d1f13d2d69b30bbe54bc7b2856391570878e3
StripesOO7 <54711792+StripeesOO7@users.noreply.github.com>
2023-06-07 04:02:35 +0200

    mappen items for auto tracking

--- scripts/autotracking/item_mapping.lua
+++ scripts/autotracking/item_mapping.lua
@@ -1,151 +1,304 @@
 -- use this file to map the AP item ids to your items
 -- first value is the code of the target item and the second is the item type (currently only "toggle", "progressive", ""and "toggle", ""but feel free to expand for your needs!)
 -- here are the SM items as an example: https://github.com/Cyb3RGER/sm_ap_tracker/blob/main/scripts/autotracking/item_mapping.lua
-ITEM_MAPPING = {
-[11],     {"Bow", ""}
-[100],    {"Progressive Bow", ""}
-[101],    {"Progressive Bow (Alt)", ""}
-[88],     {"Silver Arrows", ""}
-[59],     {"Silver Bow", ""}
-[29],     {"Book of Mudora", ""}
-[9],      {"Hammer", ""}
-[10],     {"Hookshot", ""}
-[26],     {"Magic Mirror", ""}
-[20],     {"Flute", ""}
-[75],     {"Pegasus Boots", ""}
-[27],     {"Power Glove", ""}
-[25],     {"Cape", ""}
-[41],     {"Mushroom", ""}
-[19],     {"Shovel", ""}
-[18],     {"Lamp", ""}
-[13],     {"Magic Powder", ""}
-[31],     {"Moon Pearl", ""}
-[21],     {"Cane of Somaria", ""}
-[7],      {"Fire Rod", ""}
-[30],     {"Flippers", ""}
-[8],      {"Ice Rod", ""}
-[28],     {"Titans Mitts", ""}
-[15],     {"Bombos", ""}
-[16],     {"Ether", ""}
-[17],     {"Quake", ""}
-[22],     {"Bottle", ""}
-[43],     {"Bottle (Red Potion)", ""}
-[44],     {"Bottle (Green Potion)", ""}
-[45],     {"Bottle (Blue Potion)", ""}
-[61],     {"Bottle (Fairy)", ""}
-[60],     {"Bottle (Bee)", ""}
-[72],     {"Bottle (Good Bee)", ""}
-[80],     {"Master Sword", ""}
-[2],      {"Tempered Sword", ""}
-[73],     {"Fighter Sword", ""}
-[3],      {"Golden Sword", ""}
-[94],     {"Progressive Sword", ""}
-[97],     {"Progressive Glove", ""}
-[106],    {"Triforce", ""}
-[107],    {"Power Star", ""}
-[108],    {"Triforce Piece", ""}
-[67],     {"Single Arrow", ""}
-[68],     {"Arrows (10)", ""}
-[84],     {"Arrow Upgrade (+10)", ""}
-[83],     {"Arrow Upgrade (+5)", ""}
-[39],     {"Single Bomb", ""}
-[40],     {"Bombs (3)", ""}
-[49],     {"Bombs (10)", ""}
-[82],     {"Bomb Upgrade (+10)", ""}
-[81],     {"Bomb Upgrade (+5)", ""}
-[34],     {"Blue Mail", ""}
-[35],     {"Red Mail", ""}
-[96],     {"Progressive Mail", ""}
-[12],     {"Blue Boomerang", ""}
-[42],     {"Red Boomerang", ""}
-[4],      {"Blue Shield", ""}
-[5],      {"Red Shield", ""}
-[6],      {"Mirror Shield", ""}
-[95],     {"Progressive Shield", ""}
-[33],     {"Bug Catching Net", ""}
-[24],     {"Cane of Byrna", ""}
-[62],     {"Boss Heart Container", ""}
-[63],     {"Sanctuary Heart Container", ""}
-[23],     {"Piece of Heart", ""}
-[52],     {"Rupee (1)", ""}
-[53],     {"Rupees (5)", ""}
-[54],     {"Rupees (20)", ""}
-[65],     {"Rupees (50)", ""}
-[64],     {"Rupees (100)", ""}
-[70],     {"Rupees (300)", ""}
-[89],     {"Rupoor", ""}
-[91],     {"Red Clock", ""}
-[92],     {"Blue Clock", ""}
-[93],     {"Green Clock", ""}
-[98],     {"Single RNG", ""}
-[99],     {"Multi RNG", ""}
-[78],     {"Magic Upgrade (1/2)", ""}
-[79],     {"Magic Upgrade (1/4)", ""}
-[162],    {"Small Key (Eastern Palace)", ""}
-[157],    {"Big Key (Eastern Palace)", ""}
-[141],    {"Compass (Eastern Palace)", ""}
-[125],    {"Map (Eastern Palace)", ""}
-[163],    {"Small Key (Desert Palace)", ""}
-[156],    {"Big Key (Desert Palace)", ""}
-[140],    {"Compass (Desert Palace)", ""}
-[124],    {"Map (Desert Palace)", ""}
-[170],    {"Small Key (Tower of Hera)", ""}
-[149],    {"Big Key (Tower of Hera)", ""}
-[133],    {"Compass (Tower of Hera)", ""}
-[117],    {"Map (Tower of Hera)", ""}
-[160],    {"Small Key (Hyrule Castle)", ""}
-[159],    {"Big Key (Hyrule Castle)", ""}
-[143],    {"Compass (Hyrule Castle)", ""}
-[127],    {"Map (Hyrule Castle)", ""}
-[164],    {"Small Key (Agahnims Tower)", ""}
-[155],    {"Big Key (Agahnims Tower)", ""}
-[139],    {"Compass (Agahnims Tower)", ""}
-[123],    {"Map (Agahnims Tower)", ""}
-[166],    {"Small Key (Palace of Darkness)", ""}
-[153],    {"Big Key (Palace of Darkness)", ""}
-[137],    {"Compass (Palace of Darkness)", ""}
-[121],    {"Map (Palace of Darkness)", ""}
-[171],    {"Small Key (Thieves Town)", ""}
-[148],    {"Big Key (Thieves Town)", ""}
-[132],    {"Compass (Thieves Town)", ""}
-[116],    {"Map (Thieves Town)", ""}
-[168],    {"Small Key (Skull Woods)", ""}
-[151],    {"Big Key (Skull Woods)", ""}
-[135],    {"Compass (Skull Woods)", ""}
-[119],    {"Map (Skull Woods)", ""}
-[165],    {"Small Key (Swamp Palace)", ""}
-[154],    {"Big Key (Swamp Palace)", ""}
-[138],    {"Compass (Swamp Palace)", ""}
-[122],    {"Map (Swamp Palace)", ""}
-[169],    {"Small Key (Ice Palace)", ""}
-[150],    {"Big Key (Ice Palace)", ""}
-[134],    {"Compass (Ice Palace)", ""}
-[118],    {"Map (Ice Palace)", ""}
-[167],    {"Small Key (Misery Mire)", ""}
-[152],    {"Big Key (Misery Mire)", ""}
-[136],    {"Compass (Misery Mire)", ""}
-[120],    {"Map (Misery Mire)", ""}
-[172],    {"Small Key (Turtle Rock)", ""}
-[147],    {"Big Key (Turtle Rock)", ""}
-[131],    {"Compass (Turtle Rock)", ""}
-[115],    {"Map (Turtle Rock)", ""}
-[173],    {"Small Key (Ganons Tower)", ""}
-[146],    {"Big Key (Ganons Tower)", ""}
-[130],    {"Compass (Ganons Tower)", ""}
-[114],    {"Map (Ganons Tower)", ""}
-[175],    {"Small Key (Universal)", ""}
-[90],     {"Nothing", ""}
-[176],    {"Bee Trap", ""}
-[177],    {"Faerie", ""}
-[178],    {"Good Bee", ""}
-[179],    {"Magic Jar", ""}
-[180],    {"Apple", ""}
-[46],     {"Red Potion", ""}
-[47],     {"Green Potion", ""}
-[48],     {"Blue Potion", ""}
-[14],     {"Bee", ""}
-[66],     {"Small Heart", ""}
-[74],     {"Activated Flute", ""}
+ITEM_mapPING = {
+[11]   =   {"bow+arrow", "toggle"}
+[100]   =   {"bow", "progressive"}
+[101]   =   {"bow", "progressive"}
+[88]   =   {"Silver Arrows", ""}
+[59]   =   {"bow+silvers", "toggle"}
+[29]   =   {"book", "toggle"}
+[9]   =    {"hammer", "toggle"}
+[10]   =   {"hookshot", "toggle"}
+[26]   =   {"mirror", "toggle"}
+[20]   =   {"flute", "toggle"}
+[75]   =   {"boots", "toggle"}
+[27]   =   {"powergloves", "toggle"}
+[25]   =   {"cape", "toggle"}
+[41]   =   {"mushroom", "progressive"}
+[19]   =   {"shovel", "progressive"}
+[18]   =   {"lamp", "toggle"}
+[13]   =   {"powder", "progressive"}
+[31]   =   {"Mpearl", "toggle"}
+[21]   =   {"somaria", "toggle"}
+[7]   =    {"firerod", "toggle"}
+[30]   =   {"flippers", "toggle"}
+[8]   =    {"icerod", "toggle"}
+[28]   =   {"titans", "toggle"}
+[15]   =   {"bombos", "toggle"}
+[16]   =   {"ether", "toggle"}
+[17]   =   {"quake", "toggle"}
+[22]   =   {"bottle", "progressive"}
+[43]   =   {"bottle", "progressive"}
+[44]   =   {"bottle", "progressive"}
+[45]   =   {"bottle", "progressive"}
+[61]   =   {"bottle", "progressive"}
+[60]   =   {"bottle", "progressive"}
+[72]   =   {"bottle", "progressive"}
+[80]   =   {"mastersword", "toggle"}
+[2]   =    {"temperedsword", "toggle"}
+[73]   =   {"fightersword", "toggle"}
+[3]   =    {"goldensword", "toggle"}
+[94]   =   {"sword", "progressive"}
+[97]   =   {"glove", "progressive"}
+[106]   =   {"Triforce", ""}
+[107]   =   {"Power Star", ""}
+[108]   =   {"triforcepiece", "progressive"}
+[67]   =   {"Single Arrow", ""}
+[68]   =   {"Arrows (10)", ""}
+[84]   =   {"Arrow Upgrade (+10)", "progressive"}
+[83]   =   {"Arrow Upgrade (+5)", "progressive"}
+[39]   =   {"Single Bomb", ""}
+[40]   =   {"Bombs (3)", ""}
+[49]   =   {"Bombs (10)", ""}
+[82]   =   {"bombs", "progressive"}
+[81]   =   {"bombs", "progressive"}
+[34]   =   {"blue_mail", "toggle"}
+[35]   =   {"red_mail", "toggle"}
+[96]   =   {"armor", "progressive"}
+[12]   =   {"blueboomerang", "toggle"}
+[42]   =   {"redboomerang", "toggle"}
+[4]   =    {"blueshield", "toggle"}
+[5]   =    {"redshield", "toggle"}
+[6]   =    {"mirrorshield", "toggle"}
+[95]   =   {"shield", "progressive"}
+[33]   =   {"bugcatchingnet", "toggle"}
+[24]   =   {"byrna", "toggle"}
+[62]   =   {"heartcontainer", "progressive"}
+[63]   =   {"heartcontainer", "progressive"}
+[23]   =   {"heartpiece", "progressive"}
+[52]   =   {"Rupee (1)", ""}
+[53]   =   {"Rupees (5)", ""}
+[54]   =   {"Rupees (20)", ""}
+[65]   =   {"Rupees (50)", ""}
+[64]   =   {"Rupees (100)", ""}
+[70]   =   {"Rupees (300)", ""}
+[89]   =   {"Rupoor", ""}
+[91]   =   {"Red Clock", ""}
+[92]   =   {"Blue Clock", ""}
+[93]   =   {"Green Clock", ""}
+[98]   =   {"Single RNG", ""}
+[99]   =   {"Multi RNG", ""}
+[78]   =   {"half_magic", "progressive"}
+[79]   =   {"quarter_magic", "progressive"}
+[162]   =   {"ep_smallkey", "progressive"}
+[157]   =   {"ep_bigkey", "toggle"}
+[141]   =   {"ep_compass", "toggle"}
+[125]   =   {"ep_map", "toggle"}
+[163]   =   {"dp_smallkey", "progressive"}
+[156]   =   {"dp_bigkey", "toggle"}
+[140]   =   {"dp_compass", "toggle"}
+[124]   =   {"dp_map", "toggle"}
+[170]   =   {"toh_smallkey", "progressive"}
+[149]   =   {"toh_bigkey", "toggle"}
+[133]   =   {"toh_compass", "toggle"}
+[117]   =   {"toh_map", "toggle"}
+[160]   =   {"hc_smallkey", "progressive"}
+[159]   =   {"hc_bigkey", "toggle"}
+[143]   =   {"hc_compass", "toggle"}
+[127]   =   {"hc_map", "toggle"}
+[164]   =   {"at_smallkey", "progressive"}
+[155]   =   {"at_bigkey", "toggle"}
+[139]   =   {"at_compass", "toggle"}
+[123]   =   {"at_map", "toggle"}
+[166]   =   {"pod_smallkey", "progressive"}
+[153]   =   {"pod_bigkey", "toggle"}
+[137]   =   {"pod_compass", "toggle"}
+[121]   =   {"pod_map", "toggle"}
+[171]   =   {"tt_smallkey", "progressive"}
+[148]   =   {"tt_bigkey", "toggle"}
+[132]   =   {"tt_compass", "toggle"}
+[116]   =   {"tt_map", "toggle"}
+[168]   =   {"sw_smallkey", "progressive"}
+[151]   =   {"sw_bigkey", "toggle"}
+[135]   =   {"sw_compass", "toggle"}
+[119]   =   {"sw_map", "toggle"}
+[165]   =   {"sp_smallkey", "progressive"}
+[154]   =   {"sp_bigkey", "toggle"}
+[138]   =   {"sp_compass", "toggle"}
+[122]   =   {"sp_map", "toggle"}
+[169]   =   {"ip_smallkey", "progressive"}
+[150]   =   {"ip_bigkey", "toggle"}
+[134]   =   {"ip_compass", "toggle"}
+[118]   =   {"ip_map", "toggle"}
+[167]   =   {"mm_smallkey", "progressive"}
+[152]   =   {"mm_bigkey", "toggle"}
+[136]   =   {"mm_compass", "toggle"}
+[120]   =   {"mm_map", "toggle"}
+[172]   =   {"tr_smallkey", "progressive"}
+[147]   =   {"tr_bigkey", "toggle"}
+[131]   =   {"tr_compass", "toggle"}
+[115]   =   {"tr_map", "toggle"}
+[173]   =   {"gt_smallkey", "progressive"}
+[146]   =   {"gt_bigkey", "toggle"}
+[130]   =   {"gt_compass", "toggle"}
+[114]   =   {"gt_map", "toggle"}
+[175]   =   {"smallkey (Universal)", "progressive"}
+[90]   =   {"Nothing", ""}
+[176]   =   {"Bee Trap", ""}
+[177]   =   {"Faerie", ""}
+[178]   =   {"Good Bee", ""}
+[179]   =   {"Magic Jar", ""}
+[180]   =   {"Apple", ""}
+[46]   =   {"Red Potion", ""}
+[47]   =   {"Green Potion", ""}
+[48]   =   {"Blue Potion", ""}
+[14]   =   {"Bee", ""}
+[66]   =   {"Small Heart", ""}
+[74]   =   {"Activated Flute", "toggle"}
+
+
+
+
+
+[11]   =   {"Bow", ""}
+[100]   =   {"Progressive Bow", ""}
+[101]   =   {"Progressive Bow (Alt)", ""}
+[88]   =   {"Silver Arrows", ""}
+[59]   =   {"Silver Bow", ""}
+[29]   =   {"Book of Mudora", ""}
+[9]   =    {"Hammer", ""}
+[10]   =   {"Hookshot", ""}
+[26]   =   {"Magic Mirror", ""}
+[20]   =   {"Flute", ""}
+[75]   =   {"Pegasus Boots", ""}
+[27]   =   {"Power Glove", ""}
+[25]   =   {"Cape", ""}
+[41]   =   {"Mushroom", ""}
+[19]   =   {"Shovel", ""}
+[18]   =   {"Lamp", ""}
+[13]   =   {"Magic Powder", ""}
+[31]   =   {"Moon Pearl", ""}
+[21]   =   {"Cane of Somaria", ""}
+[7]   =    {"Fire Rod", ""}
+[30]   =   {"Flippers", ""}
+[8]   =    {"Ice Rod", ""}
+[28]   =   {"Titans Mitts", ""}
+[15]   =   {"Bombos", ""}
+[16]   =   {"Ether", ""}
+[17]   =   {"Quake", ""}
+[22]   =   {"Bottle", ""}
+[43]   =   {"Bottle (Red Potion)", ""}
+[44]   =   {"Bottle (Green Potion)", ""}
+[45]   =   {"Bottle (Blue Potion)", ""}
+[61]   =   {"Bottle (Fairy)", ""}
+[60]   =   {"Bottle (Bee)", ""}
+[72]   =   {"Bottle (Good Bee)", ""}
+[80]   =   {"Master Sword", ""}
+[2]   =    {"Tempered Sword", ""}
+[73]   =   {"Fighter Sword", ""}
+[3]   =    {"Golden Sword", ""}
+[94]   =   {"Progressive Sword", ""}
+[97]   =   {"Progressive Glove", ""}
+[106]   =   {"Triforce", ""}
+[107]   =   {"Power Star", ""}
+[108]   =   {"Triforce Piece", ""}
+[67]   =   {"Single Arrow", ""}
+[68]   =   {"Arrows (10)", ""}
+[84]   =   {"Arrow Upgrade (+10)", ""}
+[83]   =   {"Arrow Upgrade (+5)", ""}
+[39]   =   {"Single Bomb", ""}
+[40]   =   {"Bombs (3)", ""}
+[49]   =   {"Bombs (10)", ""}
+[82]   =   {"Bomb Upgrade (+10)", ""}
+[81]   =   {"Bomb Upgrade (+5)", ""}
+[34]   =   {"Blue Mail", ""}
+[35]   =   {"Red Mail", ""}
+[96]   =   {"Progressive Mail", ""}
+[12]   =   {"Blue Boomerang", ""}
+[42]   =   {"Red Boomerang", ""}
+[4]   =    {"Blue Shield", ""}
+[5]   =    {"Red Shield", ""}
+[6]   =    {"Mirror Shield", ""}
+[95]   =   {"Progressive Shield", ""}
+[33]   =   {"Bug Catching Net", ""}
+[24]   =   {"Cane of Byrna", ""}
+[62]   =   {"Boss Heart Container", ""}
+[63]   =   {"Sanctuary Heart Container", ""}
+[23]   =   {"Piece of Heart", ""}
+[52]   =   {"Rupee (1)", ""}
+[53]   =   {"Rupees (5)", ""}
+[54]   =   {"Rupees (20)", ""}
+[65]   =   {"Rupees (50)", ""}
+[64]   =   {"Rupees (100)", ""}
+[70]   =   {"Rupees (300)", ""}
+[89]   =   {"Rupoor", ""}
+[91]   =   {"Red Clock", ""}
+[92]   =   {"Blue Clock", ""}
+[93]   =   {"Green Clock", ""}
+[98]   =   {"Single RNG", ""}
+[99]   =   {"Multi RNG", ""}
+[78]   =   {"Magic Upgrade (1/2)", ""}
+[79]   =   {"Magic Upgrade (1/4)", ""}
+[162]   =   {"smallkey", ""}
+[157]   =   {"bigkey", ""}
+[141]   =   {"compass", ""}
+[125]   =   {"map", ""}
+[163]   =   {"smallkey", ""}
+[156]   =   {"bigkey", ""}
+[140]   =   {"compass", ""}
+[124]   =   {"map", ""}
+[170]   =   {"smallkey", ""}
+[149]   =   {"bigkey", ""}
+[133]   =   {"compass", ""}
+[117]   =   {"map", ""}
+[160]   =   {"smallkey", ""}
+[159]   =   {"bigkey", ""}
+[143]   =   {"compass", ""}
+[127]   =   {"map", ""}
+[164]   =   {"smallkey", ""}
+[155]   =   {"bigkey", ""}
+[139]   =   {"compass", ""}
+[123]   =   {"map", ""}
+[166]   =   {"smallkey", ""}
+[153]   =   {"bigkey", ""}
+[137]   =   {"compass", ""}
+[121]   =   {"map", ""}
+[171]   =   {"smallkey", ""}
+[148]   =   {"bigkey", ""}
+[132]   =   {"compass", ""}
+[116]   =   {"map", ""}
+[168]   =   {"smallkey", ""}
+[151]   =   {"bigkey", ""}
+[135]   =   {"compass", ""}
+[119]   =   {"map", ""}
+[165]   =   {"smallkey", ""}
+[154]   =   {"bigkey", ""}
+[138]   =   {"compass", ""}
+[122]   =   {"map", ""}
+[169]   =   {"smallkey", ""}
+[150]   =   {"bigkey", ""}
+[134]   =   {"compass", ""}
+[118]   =   {"map", ""}
+[167]   =   {"smallkey", ""}
+[152]   =   {"bigkey", ""}
+[136]   =   {"compass", ""}
+[120]   =   {"map", ""}
+[172]   =   {"smallkey", ""}
+[147]   =   {"bigkey", ""}
+[131]   =   {"compass", ""}
+[115]   =   {"map", ""}
+[173]   =   {"smallkey", ""}
+[146]   =   {"bigkey", ""}
+[130]   =   {"compass", ""}
+[114]   =   {"map", ""}
+[175]   =   {"smallkey (Universal)", ""}
+[90]   =   {"Nothing", ""}
+[176]   =   {"Bee Trap", ""}
+[177]   =   {"Faerie", ""}
+[178]   =   {"Good Bee", ""}
+[179]   =   {"Magic Jar", ""}
+[180]   =   {"Apple", ""}
+[46]   =   {"Red Potion", ""}
+[47]   =   {"Green Potion", ""}
+[48]   =   {"Blue Potion", ""}
+[14]   =   {"Bee", ""}
+[66]   =   {"Small Heart", ""}
+[74]   =   {"Activated Flute", ""}
+
+
+
+
     [3860226] = {"stonebadge", "toggle"},
     [3860227] = {"knucklebadge", "toggle"},
     [3860228] = {"dynamobadge", "toggle"},
