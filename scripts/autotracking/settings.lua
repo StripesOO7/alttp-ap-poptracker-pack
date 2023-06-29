bdbb0bb42f2d63688769fcfde9e41f057cf4500e
StripesOO7 <54711792+StripeesOO7@users.noreply.github.com>
2023-06-30 01:52:46 +0200

    typo fix and prepare for usb2snes tracking

--- scripts/autotracking/settings.lua
+++ scripts/autotracking/settings.lua
@@ -1,5 +1,5 @@
 ------------------------------------------------------------------
 -- Configuration options for scripted systems in this pack
 ------------------------------------------------------------------
-AUTOTRACKER_ENABLE_ITEM_TRACKING = false
-AUTOTRACKER_ENABLE_LOCATION_TRACKING = false
+AUTOTRACKER_ENABLE_ITEM_TRACKING = true
+AUTOTRACKER_ENABLE_LOCATION_TRACKING = true
