aa66f902cde96f5aaa1776ce1468dd1853319239
StripesOO7 <54711792+StripeesOO7@users.noreply.github.com>
2024-03-16 18:54:22 +0100

    HC key logic. Castle Escape might be wonky

--- scripts/logic/HC.lua
+++ scripts/logic/HC.lua
@@ -23,25 +23,85 @@ hc_back_hall:connect_two_ways(hc_map_chest_room)
 hc_map_chest_room:connect_one_way("HC - Map Chest")
 hc_map_chest_room:connect_one_way("HC - Map Guard Key Drop")
 
-hc_map_chest_room:connect_two_ways(hc_boomerang_chest_room)
+hc_map_chest_room:connect_two_ways(hc_boomerang_chest_room, function() 
+    return any(
+        has("standard"),
+        smallKeys("hc", 0, 0, 1, 3)
+    )
+end)
 hc_boomerang_chest_room:connect_one_way("HC - Boomerang Chest")
 hc_boomerang_chest_room:connect_one_way("HC - Booomerang Guard Key Drop")
 
-dp_map_chest_room:connect_two_ways(hc_ball_guard_room)
+dp_map_chest_room:connect_two_ways(hc_ball_guard_room, function() 
+    return any(
+        has("standard"),
+        smallKeys("hc", 0, 0, 2, 4)
+    )
+end)
 hc_ball_guard_room:connect_one_way("HC - Big Key")
 
-hc_ball_guard_room:connect_two_ways(hc_zeldas_cell)
+hc_ball_guard_room:connect_two_ways(hc_zeldas_cell, function() return has("hc_bigkey") end)
 hc_zeldas_cell:connect_one_way("HC - Zelda's Chest")
 
-ce_entrance:connect_two_ways(ce_dark_cross)
+ce_entrance:connect_two_ways(ce_dark_cross, function() 
+    return any(
+        darkRooms(),
+        has("standard")
+    )
+end)
 
-ce_dark_cross:connect_two_ways(ce_rat_key_room)
+ce_dark_cross:connect_two_ways(ce_rat_key_room, function() 
+    return any(
+        all(
+            darkRooms()
+            small_keys("hc", 1, 1, 1, 3),
+        )
+        all(
+            darkRooms(),
+            small_keys("hc", 1, 1, 1, 3),
+            has("glove")
+        ),
+        has("standard")
+    ) 
+end)
 ce_dark_cross:connect_one_way("CE - Dark sross")
 
-ce_rat_key_room:connect_two_ways(ce_dropdown_entrance)
+-- ce_rat_key_room:connect_two_ways(ce_dropdown_entrance) 
+ce_rat_key_room:connect_two_ways(ce_dropdown_entrance, function() 
+    return any(
+        all(
+            darkRooms(),
+            smallKeys("hc", 1, 1, 2, 4)
+            any(
+                has("bomb"),
+                has("boots")
+            )
+        ),
+        all(
+            has("standard")
+            smallKeys("hc", 1, 1, 2, 4)
+            any(
+                has("bomb"),
+                has("boots")
+            )
+        ),
+        all(
+            has("golve")
+            any(
+                has("bomb"),
+                has("boots")
+            )
+        ),
+    ) 
+end)
 ce_rat_key_room:connect_one_way("CE - Rat Key Drop")
 
-ce_dropdown_entrance:connect_two_ways(ce_secret_room)
+ce_dropdown_entrance:connect_two_ways(ce_secret_room, function() 
+    return any(
+        has("boots"), 
+        has("bombs")
+    )
+end)
 ce_secret_room:connect_one_way("CE - Secret Room Left")
 ce_secret_room:connect_one_way("CE - Secret Room Center")
 ce_secret_room:connect_one_way("CE - Secret Room Right")
