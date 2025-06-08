877fee15e7db7b6c7f0e1a50512ee800c6fb7521
StripesOO7 <54711792+StripesOO7@users.noreply.github.com>
2025-06-08 02:11:33 +0200

    .

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -18,7 +18,7 @@ local THIRDSTAGE = {
     [35] = 35 --red mail
 }
 if Highlight then
-    local highlight_lvl= {
+    highlight_lvl= {
         [0] = Highlight.Unspecified,
         [10] = Highlight.NoPriority,
         [20] = Highlight.Avoid,
