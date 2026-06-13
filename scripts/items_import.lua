cc5ee077dbf57c2e731e382b4bc2e1a1f3c3f3c9
StripesOO7 <54711792+StripesOO7@users.noreply.github.com>
2026-06-13 11:52:38 +0200

    introduce ALTTPR Race Mode variant that diables auto map switching and auto ER tracking

--- scripts/items_import.lua
+++ scripts/items_import.lua
@@ -6,3 +6,15 @@ Tracker:AddItems("items/dungeon_bosses.json")
 Tracker:AddItems("items/dungeon_items.json")
 Tracker:AddItems("items/doors_static_images.json")
 Tracker:AddItems("items/shops_bottles_pulls.json")
+
+
+local variant = Tracker.ActiveVariantUID
+
+if variant == "Map Tracker ALTTPR Race Mode - SNES" then
+    local ER_tracking = Tracker:FindObjectForCode("er_tracking_method") --[[@as JsonItem]]
+    ER_tracking.Active = false
+    ER_tracking.IgnoreUserInput = true
+    local Map_switch = Tracker:FindObjectForCode("ui_hint") --[[@as JsonItem]]
+    Map_switch.Active = false
+    Map_switch.IgnoreUserInput = true
+end
